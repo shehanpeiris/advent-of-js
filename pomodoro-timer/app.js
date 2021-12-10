00bb3714b8eaac729d8d4f639313886f45495317
Shehan Peiris <msp730@orad.northwestern.edu>
2021-12-10 16:25:39 -0600

    Start button text turns on and off

--- pomodoro-timer/app.js
+++ pomodoro-timer/app.js
@@ -1,11 +1,24 @@
 // Adding event listener to "Start" button
 const startButton = document.getElementById("start-button");
+let timerRunning = false;
 
-// ClickHandler function once "Start" button is clicked
-const startClickHandler = () => {
-    console.log("You clicked start!");
+const runTimer = () => {
     startButton.innerHTML = "Stop";
+    timerRunning = true;
 };
 
-startButton.addEventListener("click", startClickHandler);
+const stopTimer = () => {
+    startButton.innerHTML = "Start";
+    timerRunning = false;
+};
+
+
+// ClickHandler function once "Start" button is clicked
+const clickHandler = () => {
+    console.log("You clicked start!");
+    timerRunning ? stopTimer() : runTimer();
+};
+
+
+startButton.addEventListener("click", clickHandler);
 
