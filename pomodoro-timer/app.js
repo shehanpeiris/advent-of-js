0ffc619aeaded42e2a0a4eea93e93c7d57d15098
Shehan Peiris <msp730@orad.northwestern.edu>
2021-12-14 13:17:06 -0600

    modal to update timer settings is functional

--- pomodoro-timer/app.js
+++ pomodoro-timer/app.js
@@ -1,5 +1,9 @@
 // Definitions of elements or values
 const timerButton = document.getElementById("timer-button");
+const settingsButton = document.getElementById("settings-button");
+const settingsModal = document.getElementById("settings-modal");
+const modalClose = document.querySelector(".close-btn");
+const updateTimer = document.getElementById("update-timer-btn");
 let minutesValue = parseInt(document.querySelector("#minutes").value);
 let secondsValue = parseInt(document.querySelector("#seconds").value);
 let minutesText = document.querySelector("#minutes");
@@ -41,5 +45,30 @@ const stopTimer = () => {
     timerBoolean = false;
 };
 
+// Function for settings button
+const openSettings = () => {
+    settingsModal.style.display = "block";
+};
+
+const closeSettings = () => {
+    settingsModal.style.display = "none";
+};
+
+const updateSettings = () => {
+    minutesValue = document.getElementById("minutes-input").value;
+    secondsValue = document.getElementById("seconds-input").value;
+    minutesText.value = minutesValue;
+    secondsText.value = secondsValue < 10 ? "0" + secondsValue : secondsValue;
+    closeSettings();
+};
+
 // When you click the timer button, fire the runTimer function
 timerButton.addEventListener("click", runTimer);
+
+// When you click the settings button, fire the openSettings function
+settingsButton.addEventListener("click", openSettings);
+
+// When you click the x on the modal, close the modal
+modalClose.addEventListener("click", closeSettings);
+
+updateTimer.addEventListener("click", updateSettings);
